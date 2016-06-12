a1a3e19cf67cbf2b42091534a9bbfaf2cd16a049
Flowdeeps <speedwolf@gmail.com>
2016-06-12 23:34:12 +0100

    Added first pass at building file from md and partials

--- flatearth.py
+++ flatearth.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 
-import Pygments
+import os
+import markdown
+
+input_dir = 'content'
+output_dir = 'output'
+
+md = markdown.Markdown( output_format = "html5" )
+
+items = os.listdir( input_dir )
+for item in items:
+    if item.lower().endswith( '.md' ):
+        input_md = item
+
+input_md = input_dir + "/" + input_md
+input_md = open( input_md, 'r' ).read()
+input_md = md.convert( input_md )
+
+body = input_md
+
+head = open( 'content/assets/tpl/header.html', 'r' ).read()
+aside = open( 'content/assets/tpl/aside.html', 'r' ).read()
+foot = open( 'content/assets/tpl/footer.html', 'r' ).read()
+
+output_file = open( output_dir + '/index.html', 'w' )
+output_file.write( head )
+output_file.write( body )
+output_file.write( aside )
+output_file.write( foot )
+output_file.close()
