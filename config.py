99f21db677cf7e7ddb8d82c2962a637685a10f36
Pete Haughie <pete.haughie@sourcefabric.org>
2016-06-16 15:29:12 +0100

    Abstracted out all the config stuff. Config is now all generated from the ini and retrieved from named key value pairs.

---
+++ config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import configparser
+
+def config ( input ):
+
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read( input )
+    config.sections()
+    config_dict={}
+
+    for section in config:
+        for var in config[ section ]:
+            config_dict[ var ] = config[ section ][ var ]
+
+    if ( config_dict[ "output_string" ] != None ):
+        config_dict[ "output_string" ] = "<!-- " + config_dict[ "output_string" ] + " -->"
+
+    return config_dict
