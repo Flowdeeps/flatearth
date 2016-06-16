e1075352cef5972f3fd60e177807a436b8337f0d
Pete Haughie <pete.haughie@sourcefabric.org>
2016-06-16 16:03:02 +0100

    Added short description of how the config is generated and what it returns

--- flatearth.py
+++ flatearth.py
@@ -2,65 +2,29 @@
 
 from __future__ import print_function
 
-import os
-import configparser
+import os, markdown, shutil
 
-import markdown
 from bs4 import BeautifulSoup
-
 from githistory import githistory
-
 from PIL import Image
+from config import config # this is from config.py - feed it the ini_file string and it will return my_config with named keys - eg: my_config[ "your_config_parameter" ]
 
-import shutil
+ini_file = "general.ini"
 
-config = configparser.ConfigParser()
-config.sections()
-config.read( 'general.ini' )
-config.sections()
+my_config = config( ini_file )
 
-input_folder = config[ 'APP' ][ 'inputfolder' ]
-document_folder = config[ 'APP' ][ 'docfolder' ] # why is this specified independently? Because you might want a seperation of theme and content.
-output_folder = config[ 'APP' ][ 'outputfolder' ]
-output_filename = output_folder + config[ 'APP' ][ 'outputfile' ]
-output_string = config[ 'APP' ][ 'outputstring' ]
-output_string = "<!-- " + output_string + " -->"
-
-# asset folders
-assets = config[ 'SITE' ][ 'assets' ]
-tpl_folder = input_folder + assets + config[ 'TPL' ][ 'tplfolder' ]
-css_folder = assets + config[ 'CSS' ][ 'cssfolder' ]
-js_folder = assets + config[ 'JS' ][ 'jsfolder' ]
-img_folder = config[ 'IMG' ][ 'imgfolder' ]
-# asset files
-# tpl files
-tpl_head_file = tpl_folder + config[ 'TPL' ][ 'header' ]
-tpl_aside_file = tpl_folder + config[ 'TPL' ][ 'aside' ]
-tpl_foot_file = tpl_folder + config[ 'TPL' ][ 'footer' ]
-# css file
-css_file = css_folder + config[ 'CSS' ]['cssfile']
-# js file
-js_file = js_folder + config[ 'JS' ][ 'jsfile' ]
-# img sizes
-img_max = config[ 'IMG' ][ 'imgmax' ]
-img_mid = config[ 'IMG' ][ 'imgmid' ]
-img_min = config[ 'IMG' ][ 'imgmin' ]
-# picture breakpoints
-# there's only two as the lareg image is the default img src
-break_mid = config['IMG']['breakmid']
-break_min = config['IMG']['breakmin']
-
-md_items = os.listdir( document_folder )
+md_items = os.listdir( my_config[ "document_folder" ] )
 for md_item in md_items:
     if md_item.lower().endswith( '.md' ):
         input_md = md_item
 
 md = markdown.Markdown( output_format = "html5" )
 
-input_md = document_folder + input_md
+input_md = my_config[ "document_folder" ] + input_md
 input_md = open( input_md, 'r' ).read()
 input_md = md.convert( input_md )
-soup = BeautifulSoup( input_md, 'html5lib' )
+soup = BeautifulSoup( input_md, 'html.parser' )
+
 for html_elem in soup.find_all( 'h1' ):
     title_entity = html_elem.text
 for img_elem in soup.find_all( 'img' ):
@@ -79,33 +43,33 @@ for img_elem in soup.find_all( 'img' ):
     while i < 2:
         if i == 1:
             new_picture_src1 = soup.new_tag( 'source' )
-            new_picture_src1[ 'media' ] = "( min-width: " + break_mid + "px )"
+            new_picture_src1[ 'media' ] = "( min-width: " + my_config[ "break_mid" ] + "px )"
             new_picture_src1[ 'srcset' ] = picture_src
             new_picture_entity_parent.insert( 1, new_picture_src1 )
         else:
             new_picture_src2 = soup.new_tag( 'source' )
-            new_picture_src2[ 'media' ] = "( min-width: " + break_min + "px )"
+            new_picture_src2[ 'media' ] = "( min-width: " + my_config[ "break_min" ] + "px )"
             new_picture_src2[ 'srcset' ] = picture_src
             new_picture_entity_parent.insert( 1, new_picture_src2 )
         i = i + 1
     soup = str( soup ).replace( "</source>", "" ) # it's dirty but we've already performed our transformations
-print( soup )
+# print( soup )
 
 body = soup + '\n' # I like to have a new line at the end of all my html documents
-
-head = open( tpl_head_file, 'r' ).read()
+# body = body.replace( "<body><html><head></head><body>", "" )
+head = open( my_config[ "input_folder" ] + my_config[ "assets_folder" ] + my_config[ "tpl_folder" ] + my_config[ "tpl_head" ], 'r' ).read()
 head = head.replace( "$title", title_entity )
-head = head.replace( "$css", css_file )
-head = head.replace( "$js", js_file )
+head = head.replace( "$css", my_config[ "input_folder" ] + my_config[ "assets_folder" ] + my_config[ "css_folder" ] + my_config[ "css_file" ] )
+head = head.replace( "$js", my_config[ "js_file" ] )
 
-aside = open( tpl_aside_file, 'r' ).read()
-foot = open( tpl_foot_file, 'r' ).read()
+aside = open( my_config[ "input_folder" ] + my_config[ "assets_folder" ] + my_config[ "tpl_folder" ] + my_config[ "tpl_aside" ], 'r' ).read()
+foot = open( my_config[ "input_folder" ] + my_config[ "assets_folder" ] + my_config[ "tpl_folder" ] + my_config[ "tpl_footer" ], 'r' ).read()
 
 # githistory( "documents/index.md" ) # this is where we get our document history from
 
 # check for css
-new_css_folder = output_folder + css_folder
-css_items = os.listdir( input_folder + css_folder )
+new_css_folder = my_config[ "output_folder" ] + my_config[ "css_folder" ]
+css_items = os.listdir( my_config[ "input_folder" ] + my_config[ "assets_folder" ] + my_config[ "css_folder" ] )
 
 i = 0
 for css_item in css_items:
@@ -114,14 +78,12 @@ for css_item in css_items:
         if i > 0:
             if not os.path.exists( new_css_folder ):
                 os.makedirs( new_css_folder )
-            shutil.copy2( input_folder + css_folder + css_item, new_css_folder )
+            shutil.copy2( my_config[ "input_folder" ] + my_config[ "assets_folder" ] + my_config[ "css_folder" ] + css_item, new_css_folder )
 
-
-
-output_file = open( output_filename, 'w' )
+output_file = open( my_config[ "output_file" ], 'w' )
 output_file.write( head )
 output_file.write( '\n' )
-output_file.write( output_string )
+output_file.write( my_config[ "output_string" ] )
 output_file.write( '\n' )
 output_file.write( "<body>" )
 output_file.write( body )
